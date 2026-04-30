436d97618486c65d78587877ac77fc3e36fc68fb
Jerónimo Falcón <jfalcondavila42@gmail.com>
2026-04-30 16:06:01 +0100

    Resolve merge conflict of init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,87 +1,41 @@
 -- ENTIDADES DE LA APLICACIÓN
 
-<<<<<<< HEAD
-CREATE TABLE users (
-                       name VARCHAR(100) NOT NULL,
-                       mail VARCHAR(255) NOT NULL,
-                       password VARCHAR(255) NOT NULL,
-=======
 CREATE TABLE users
 (
     name     VARCHAR(100) NOT NULL,
     mail     VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
->>>>>>> feat-modify-button
 
-                       CONSTRAINT pk_users PRIMARY KEY (mail)
+    CONSTRAINT pk_users PRIMARY KEY (mail)
 );
 
 
-<<<<<<< HEAD
-CREATE TABLE board (
-                       board_id INT AUTO_INCREMENT NOT NULL,
-                       name VARCHAR(100) NOT NULL,
-=======
 CREATE TABLE board
 (
     board_id INT AUTO_INCREMENT NOT NULL,
     name     VARCHAR(100) NOT NULL,
->>>>>>> feat-modify-button
 
-                       CONSTRAINT pk_board PRIMARY KEY (board_id)
+    CONSTRAINT pk_board PRIMARY KEY (board_id)
 );
 
 
-<<<<<<< HEAD
-CREATE TABLE columns_table (
-                               column_id INT AUTO_INCREMENT NOT NULL,
-                               name VARCHAR(100) NOT NULL,
-=======
 CREATE TABLE columns_table
 (
     column_id INT AUTO_INCREMENT NOT NULL,
     name      VARCHAR(100) NOT NULL,
->>>>>>> feat-modify-button
 
-                               CONSTRAINT pk_column PRIMARY KEY (column_id)
+    CONSTRAINT pk_column PRIMARY KEY (column_id)
 );
 
 -- RELACIONES ENTRE ENTIDADES DE LA APLICACIÓN
 
-<<<<<<< HEAD
-CREATE TABLE users_board (
-                             user_mail VARCHAR(255),
-                             board_id INT,
-=======
 CREATE TABLE users_board
 (
     user_mail VARCHAR(255),
     board_id  INT,
->>>>>>> feat-modify-button
 
-                             CONSTRAINT pk_users_board PRIMARY KEY (user_mail, board_id),
+    CONSTRAINT pk_users_board PRIMARY KEY (user_mail, board_id),
 
-<<<<<<< HEAD
-                             CONSTRAINT fk_ub_user FOREIGN KEY (user_mail) REFERENCES users(mail) ON DELETE CASCADE,
-                             CONSTRAINT fk_ub_board FOREIGN KEY (board_id) REFERENCES board(board_id) ON DELETE CASCADE
-);
-
-CREATE TABLE column_task(
-                            id_column INT NOT NULL,
-
-                            id_task INT AUTO_INCREMENT NOT NULL,
-                            name VARCHAR(100) NOT NULL,
-                            description VARCHAR(255),
-                            date DATETIME,
-
-                            CONSTRAINT pk_task PRIMARY KEY (id_task),
-                            CONSTRAINT fk_ct_column  FOREIGN KEY (id_column) REFERENCES columns_table(column_id) ON DELETE CASCADE
-);
-
-CREATE TABLE user_task (
-                           user_mail VARCHAR(255),
-                           task_id INT,
-=======
     CONSTRAINT fk_ub_user FOREIGN KEY (user_mail) REFERENCES users (mail) ON DELETE CASCADE,
     CONSTRAINT fk_ub_board FOREIGN KEY (board_id) REFERENCES board (board_id) ON DELETE CASCADE
 );
@@ -104,36 +58,15 @@ CREATE TABLE user_task
 (
     user_mail VARCHAR(255),
     task_id   INT,
->>>>>>> feat-modify-button
 
-                           CONSTRAINT pk_user_task PRIMARY KEY (user_mail, task_id),
+    CONSTRAINT pk_user_task PRIMARY KEY (user_mail, task_id),
 
-<<<<<<< HEAD
-                           CONSTRAINT fk_ut_user  FOREIGN KEY (user_mail) REFERENCES users(mail) ON DELETE CASCADE,
-                           CONSTRAINT fk_ut_task FOREIGN KEY (task_id)  REFERENCES column_task(id_task) ON DELETE CASCADE
-=======
     CONSTRAINT fk_ut_user FOREIGN KEY (user_mail) REFERENCES users (mail) ON DELETE CASCADE,
     CONSTRAINT fk_ut_task FOREIGN KEY (task_id) REFERENCES column_task (id_task) ON DELETE CASCADE
->>>>>>> feat-modify-button
 );
 
 -- propiedad multivaluada de "task"
 
-<<<<<<< HEAD
-CREATE TABLE task_labels (
-                             id_task_label INT AUTO_INCREMENT NOT NULL,
-                             label VARCHAR(100) NOT NULL,
-                             task_id INT NOT NULL,
-
-                             CONSTRAINT pk_task_label PRIMARY KEY (id_task_label),
-                             CONSTRAINT fk_tl_task FOREIGN KEY (task_id) REFERENCES column_task(id_task) ON DELETE CASCADE
-);
-
-
-CREATE TABLE board_column(
-                             id_board INT NOT NULL,
-                             id_column INT NOT NULL,
-=======
 CREATE TABLE task_labels
 (
     id_task_label INT AUTO_INCREMENT NOT NULL,
@@ -149,70 +82,46 @@ CREATE TABLE board_column
 (
     id_board  INT NOT NULL,
     id_column INT NOT NULL,
->>>>>>> feat-modify-button
 
-                             CONSTRAINT pk_board_column PRIMARY KEY (id_board, id_column),
+    CONSTRAINT pk_board_column PRIMARY KEY (id_board, id_column),
 
-<<<<<<< HEAD
-                             CONSTRAINT fk_bc_board  FOREIGN KEY (id_board) REFERENCES board(board_id) ON DELETE CASCADE,
-                             CONSTRAINT fk_bc_column FOREIGN KEY (id_column)  REFERENCES columns_table(column_id) ON DELETE CASCADE
-=======
     CONSTRAINT fk_bc_board FOREIGN KEY (id_board) REFERENCES board (board_id) ON DELETE CASCADE,
     CONSTRAINT fk_bc_column FOREIGN KEY (id_column) REFERENCES columns_table (column_id) ON DELETE CASCADE
->>>>>>> feat-modify-button
 );
 
 -- Restricciones Semáticas Adicionales (RSA's)
 
 -- RSA (user_board): solo actúa si no estamos en modo eliminación de cuenta
-DELIMITER //
+DELIMITER
+//
 
 CREATE TRIGGER trigger_ub_before_delete
-<<<<<<< HEAD
-    BEFORE DELETE ON users_board
-=======
     BEFORE DELETE
     ON users_board
->>>>>>> feat-modify-button
     FOR EACH ROW
 BEGIN
     DECLARE total_users INT;
 
-<<<<<<< HEAD
     IF @disable_trigger IS NULL OR @disable_trigger = 0 THEN
-    SELECT COUNT(*) INTO total_users
-=======
     SELECT COUNT(*)
     INTO total_users
->>>>>>> feat-modify-button
     FROM users_board
     WHERE board_id = OLD.board_id;
 
     IF total_users <= 1 THEN
-<<<<<<< HEAD
             SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = 'Error: El tablero debe tener al menos un usuario.';
 END IF;
-END IF;
-END //
-
-DELIMITER ;
-
--- DATOS DE EJEMPLO
-
-INSERT INTO board (name) VALUES ('Proyecto Backend');
-=======
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Error: El tablero debe tener al menos un usuario.';
 END IF;
 END
 //
 
 DELIMITER ;
 
+-- DATOS DE EJEMPLO
+
 INSERT INTO board (name)
 VALUES ('Proyecto Backend');
->>>>>>> feat-modify-button
 
 INSERT INTO columns_table (name)
 VALUES ('To Do');
